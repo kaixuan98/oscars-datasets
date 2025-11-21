639919df8136c854fc8aa8de89bd304d995bd70b
kaixuan98 <ckaixuan3@gmail.com>
2025-11-21 18:27:17 -0500

    rotten tomato class

--- scripts/scraper_source/rotten_tomato/scraper.py
+++ scripts/scraper_source/rotten_tomato/scraper.py
@@ -1,7 +1,21 @@
-# def query_formatter(self, title: str, replacement: str = "+"):
-#     """
-#     Format the title and replace all the space with + or any replacement symbol
+from scripts.scraper_source.base_scraper import AbstractScraper
 
-#     """
-#     result = title.lower().strip().replace(" ", replacement)
-#     return result
+
+class RottenTomatoScraper(AbstractScraper):
+    souce_name = "rotten_tomato"
+    extended_columns = []
+
+    def load_to_process_data(self, input_path="data/raw/master_list.csv"):
+        return super().load_to_process_data(input_path)
+
+    def search_film(self, query):
+        return super().search_film(query)
+
+    def extract_match(self, sources, target):
+        return super().extract_match(sources, target)
+
+    def extract_score(self, source_url):
+        return super().extract_score(source_url)
+
+    def write_to_output(self, row, url, ratings):
+        return super().write_to_output(row, url, ratings)
