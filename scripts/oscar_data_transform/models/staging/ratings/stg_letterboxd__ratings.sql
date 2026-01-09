6f9fe2893bcd683f1389545f7b9e36838b4cb36e
kaixuan98 <averychin7@gmail.com>
2026-01-08 21:09:56 -0500

    keep the column naming in letterboxd data with lb

--- scripts/oscar_data_transform/models/staging/ratings/stg_letterboxd__ratings.sql
+++ scripts/oscar_data_transform/models/staging/ratings/stg_letterboxd__ratings.sql
@@ -3,11 +3,11 @@ select
     lower(film) as title_lower,
     year_film as release_year,
     year_ceremony,
-    lb_url as letterboxd_url,
-    cast(lb_rating as double) as letterboxd_rating,
-    cast(lb_rating_count as integer) as letterboxd_rating_count,
-    cast(lb_watched_by as integer) as letterboxd_watched,
-    cast(lb_liked_by as integer) as letterboxd_liked
+    lb_url,
+    cast(lb_rating as double),
+    cast(lb_rating_count as integer),
+    cast(lb_watched_by as integer),
+    cast(lb_liked_by as integer)
 from
     read_csv_auto(
         "/Users/kaixuanchin/Code/oscars-datasets/data/scraped/lb_20260102.csv"
