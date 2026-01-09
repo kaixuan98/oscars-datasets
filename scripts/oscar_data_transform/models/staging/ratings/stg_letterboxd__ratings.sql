98e4e9837fdce9c1632e7187e66d036440bb6e74
kaixuan98 <averychin7@gmail.com>
2026-01-09 17:21:10 -0500

    update letterboxd column name

--- scripts/oscar_data_transform/models/staging/ratings/stg_letterboxd__ratings.sql
+++ scripts/oscar_data_transform/models/staging/ratings/stg_letterboxd__ratings.sql
@@ -4,8 +4,8 @@ select
     cast(year_film as integer) as release_year,
     year_ceremony,
     lb_url,
-    cast(lb_rating as double) as lb_rating,
-    cast(lb_rating_count as integer) as lb_rating_count,
+    cast(lb_rating_outof as double) as lb_rating,
+    cast(lb_rating as integer) as lb_rating_count,
     cast(lb_watched_by as integer) as lb_watched_by,
     cast(lb_liked_by as integer) as lb_liked_by
 from
