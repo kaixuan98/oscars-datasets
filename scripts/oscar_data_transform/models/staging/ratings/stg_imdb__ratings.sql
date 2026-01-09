135b799dc5f23caaf9008426638ae31cebb0e199
kaixuan98 <averychin7@gmail.com>
2026-01-08 21:29:27 -0500

    feat: imdb ratings staging model

---
+++ scripts/oscar_data_transform/models/staging/ratings/stg_imdb__ratings.sql
@@ -0,0 +1,18 @@
+select
+    id as movie_id,
+    title,
+    lower(title) as title_lower,
+    release_date,
+    extract(
+        'year'
+        FROM
+            release_date
+    ) as release_year,
+    imdb_id,
+    cast(imdb_rating as integer) cast(imdb_votes as integer) as imdb_rating_count,
+from
+    read_csv_auto(
+        "/Users/kaixuanchin/Code/oscars-datasets/data/raw/TMDB_all_movies.csv"
+    )
+where
+    release_date > '1989-01-01'
