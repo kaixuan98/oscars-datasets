bb75b68fc3a11b6ba2740ec7bea700b53a3d481e
kaixuan98 <averychin7@gmail.com>
2026-01-08 21:38:19 -0500

    feat: metacritics ratings staging model

---
+++ scripts/oscar_data_transform/models/staging/ratings/stg_metacritic__ratings.sql
@@ -0,0 +1,14 @@
+select
+    film as title,
+    lower(film) as title_lower,
+    cast(year_film as integer) as release_year,
+    year_ceremony,
+    mc_url,
+    cast(mc_metascore as integer),
+    cast(lb_metascore_count as integer),
+    cast(lb_users_score as double),
+    cast(lb_users_score_count as integer)
+from
+    read_csv_auto(
+        "/Users/kaixuanchin/Code/oscars-datasets/data/scraped/mc_20260102.csv"
+    )
