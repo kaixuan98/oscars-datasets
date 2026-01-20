f5d5e4c85753bc506db52fa4975b4a0193177b38
kaixuan98 <averychin7@gmail.com>
2026-01-20 14:26:51 -0500

    update staging golden globe and enriching golden globe data with imdb id

---
+++ scripts/oscar_data_transform/models/intermediate/awards/int_golden_globes__enriched.sql
@@ -0,0 +1,9 @@
+select 
+    golden_globes.*,
+    movies.imdb_id
+from {{ ref('stg_golden_globes__awards') }} as golden_globes
+left join {{ ref('stg_tmdb__movies') }} as movies
+    on
+        golden_globes.title_lower_normalized = movies.title_lower
+        and golden_globes.ceremony_year between movies.release_year - 1 and movies.release_year + 1
+        and movies.budget > 0 and movies.runtime >= 90
