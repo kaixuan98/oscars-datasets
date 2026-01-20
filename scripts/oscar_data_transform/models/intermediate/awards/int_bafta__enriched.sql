54dad72d308048549ecb14e2ff5fb0f55a3186ad
kaixuan98 <averychin7@gmail.com>
2026-01-20 17:25:52 -0500

    feat: enrich bafta with imdb_id

---
+++ scripts/oscar_data_transform/models/intermediate/awards/int_bafta__enriched.sql
@@ -0,0 +1,31 @@
+with enriched as (
+    select 
+        bafta.*, 
+        movies.imdb_id
+    from {{ref('stg_wikipedia__bafta')}} as bafta
+    left join {{ref('stg_tmdb__movies')}} as movies 
+        on 
+            bafta.title_lower = movies.title_lower
+            and bafta.ceremony_year between movies.release_year - 1 and movies.release_year + 1
+    where ceremony_year > 1990
+), 
+
+dedup as (
+    select 
+        *,
+        row_number() over (partition by title_lower, ceremony_year
+                            order by case when imdb_id is not null then 1 else 2 end) as rn 
+    from enriched
+)
+
+select 
+    title,
+    title_lower, 
+    ceremony_year, 
+    award_body, 
+    award_category,
+    won_flag,
+    award_result, 
+    imdb_id
+from dedup
+where rn = 1
