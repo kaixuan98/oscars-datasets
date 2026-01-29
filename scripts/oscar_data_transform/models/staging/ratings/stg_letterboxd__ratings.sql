fe97faee9583a79d4044812efd40a7ec088fa12c
kaixuan98 <averychin7@gmail.com>
2026-01-29 18:01:57 -0500

    update naming for rating to rating_value

--- scripts/oscar_data_transform/models/staging/ratings/stg_letterboxd__ratings.sql
+++ scripts/oscar_data_transform/models/staging/ratings/stg_letterboxd__ratings.sql
@@ -3,7 +3,7 @@ select
     REGEXP_REPLACE(lower(film), '[^a-zA-Z0-9]', '', 'g') as title_lower,
     cast(year_film as integer) as release_year,
     lb_url,
-    cast(lb_rating_outof as double) as rating,
+    cast(lb_rating_outof as double) as rating_value,
     cast(lb_rating as integer) as rating_count,
     cast(lb_watched_by as integer) as watched_count,
     cast(lb_liked_by as integer) as liked_count
