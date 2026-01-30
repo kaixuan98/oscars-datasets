779cbc3adc59dbaf375961bb50986f613323f817
kaixuan98 <averychin7@gmail.com>
2026-01-30 14:10:02 -0500

    update awards(oscars and golden globe) stricten the join condition and add film identity overwritte seed for inconsistency naming

--- scripts/oscar_data_transform/models/intermediate/awards/int_oscars__enriched.sql
+++ scripts/oscar_data_transform/models/intermediate/awards/int_oscars__enriched.sql
@@ -1,11 +1,18 @@
 with enriched as (
     select 
-        oscars.*, 
-        movies.imdb_id
+        oscars.*,
+        coalesce(
+            overwritten.imdb_id,
+            movies.imdb_id
+        ) as imdb_id
     from {{ref('stg_kaggle__oscars')}} as oscars
+    left join {{ref('film_identity_overwrite')}} as overwritten
+        on overwritten.source_system = 'oscars'
+        and overwritten.source_title_lower = oscars.title_lower
+        and overwritten.source_year = oscars.ceremony_year
     left join {{ref('stg_tmdb__movies')}} as movies 
         on 
-            oscars.title_lower like movies.title_lower ||'%'
+            oscars.title_lower=movies.title_lower
             and oscars.ceremony_year between movies.release_year - 2 and movies.release_year + 2
     where ceremony_year > 1990 and award_category = 'best picture'
 ), 
